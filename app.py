846f24ed620e60897f30d50b229e67d35bc31458
kyle8998 <kylelim8998@gmail.com>
2017-03-09 22:37:49 -0500

    Updated Files

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ toolbar = DebugToolbarExtension(app)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'jack8998'
-app.config['MYSQL_DATABASE_DB'] = 'appgen'
+app.config['MYSQL_DATABASE_DB'] = 'db_appstore'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
@@ -54,7 +54,7 @@ def home():
 
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO application_store (name_ApplicationStore) VALUES (%s)",(name))
+        cursor.execute("INSERT INTO appstore_list (name) VALUES (%s)",(name))
 
         # me = 'testy'
         # db.session.add(me)
@@ -66,7 +66,7 @@ def home():
     elif request.method == 'GET':
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("SELECT name_ApplicationStore FROM application_store ORDER BY id_ApplicationStore DESC LIMIT 1")
+        cursor.execute("SELECT name FROM appstore_list ORDER BY id DESC LIMIT 1")
         name=cursor.fetchone()[0]
         return render_template("home.html", name=name)
 
@@ -76,29 +76,75 @@ def home():
 def createapp():
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT name_ApplicationStore FROM application_store ORDER BY id_ApplicationStore DESC LIMIT 1")
+    cursor.execute("SELECT name FROM appstore_list ORDER BY id DESC LIMIT 1")
     name=cursor.fetchone()[0]
-    return render_template("createapp.html", name=name)
+    appname = 'Application 1'
+    return render_template("createapp.html", name=name, appname=appname)
 
 @app.route('/applist/', methods = ['POST', 'GET'])
 def applist():
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute("SELECT name_ApplicationStore FROM application_store ORDER BY id_ApplicationStore DESC LIMIT 1")
-    name=cursor.fetchone()[0]
+    #This gets the appStore name which displays at the top
+    cursor.execute("SELECT name FROM appstore_list ORDER BY id DESC LIMIT 1")
+    appstore_name=cursor.fetchone()[0]
+    #This gets the appStore ID which is hidden
+    cursor.execute("SELECT id FROM appstore_list ORDER BY id DESC LIMIT 1")
+    appstore_id=cursor.fetchone()[0]
 
     if request.method == 'POST':
+        appName = request.form['appName']
         userReviews = request.form['reviews']
         downloadCount = request.form['downloadCount']
         verifiedDeveloper = request.form['verifiedDeveloper']
         paidApp = request.form['paid']
+        rating = 3.5
 
-        cursor.execute("INSERT INTO application_list (userReviews_application, downloadCount_application, verifiedDeveloper_application, paidApp_application) VALUES (%s, %s, %s, %s)",(userReviews, downloadCount, verifiedDeveloper, paidApp))
-
-
+        cursor.execute("INSERT INTO app_list (appstore_id, app_name, app_reviews, app_rating, app_download, app_verified, app_paid) VALUES (%s, %s, %s, %s, %s, %s, %s)",(appstore_id, appName, userReviews, rating, downloadCount, verifiedDeveloper, paidApp))
         conn.commit()
 
-    return render_template("applist.html", name=name)
+    cursor.execute("SELECT app_download FROM app_list ORDER BY id DESC LIMIT 1")
+    download=cursor.fetchone()[0]
+
+    return render_template("applist.html", name=appstore_name, appstore_id=appstore_id, download=download)
+
+@app.route('/editapp/')
+def editapp():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM appstore_list ORDER BY id DESC LIMIT 1")
+    name=cursor.fetchone()[0]
+
+    cursor.execute("SELECT app_reviews FROM app_list ORDER BY id DESC LIMIT 1")
+    review=cursor.fetchone()[0]
+    cursor.execute("SELECT app_download FROM app_list ORDER BY id DESC LIMIT 1")
+    download=cursor.fetchone()[0]
+    cursor.execute("SELECT app_verified FROM app_list ORDER BY id DESC LIMIT 1")
+    developer=cursor.fetchone()[0]
+    cursor.execute("SELECT app_paid FROM app_list ORDER BY id DESC LIMIT 1")
+    paid=cursor.fetchone()[0]
+
+    if review=='true':
+        setReview='active'
+        reviewOpp='false'
+    else:
+        setReview=''
+        reviewOpp='true'
+
+    if developer=='true':
+        setDeveloper='active'
+        developerOpp='false'
+    else:
+        setDeveloper=''
+        developerOpp='true'
+    if paid=='true':
+        setPaid='active'
+        paidOpp='false'
+    else:
+        setPaid=''
+        paidOpp='true'
+
+    return render_template("editapp.html", name=name, setReview=setReview, review=review, reviewOpp=reviewOpp, download=download, setDeveloper=setDeveloper, developer=developer, developerOpp=developerOpp, setPaid=setPaid, paid=paid, paidOpp=paidOpp)
 
 @app.route('/addpermissionslist/')
 def addpermissionslist():
